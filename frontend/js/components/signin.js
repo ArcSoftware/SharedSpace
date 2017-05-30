c0338dc0e5409bedc8747dc431e37870050986d0
nickFalcone <nfalcone@u.rochester.edu>
2017-05-30 18:37:20 -0400

    revert to prior commit

--- frontend/js/components/signin.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'signin',
-  array: {
-    templateUrl: "templates/signin.html",
-    controller: "signin",
-  }
-};
--- frontend/js/controllers/signin.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  name: "signin",
-  func: function ($scope, controller) {
-    $scope.userNameInput = '';
-  }
-} 
