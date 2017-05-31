8d2cf32dd29cb72a53de2cfcfc5c2ceea2c497db
nickFalcone <nfalcone@u.rochester.edu>
2017-05-31 17:22:02 -0400

    fix merge conflicts HEAD

--- frontend/js/components/signin.js
+++ frontend/js/components/signin.js
@@ -4,12 +4,5 @@ module.exports = {
 
         templateUrl: "/src/main/resources/templates/signin.html",
         controller: "SignInController",
-<<<<<<< HEAD
-=======
-
-        // bindings:{
-        //     which: "<",
-        // }
->>>>>>> d8be259efb7db341e84bf90d46831ac66d48d5b7
     }
 }
--- frontend/js/services/SignInService.js
+++ frontend/js/services/SignInService.js
@@ -1,14 +1,9 @@
 module.exports={
     name:'SignInService',
     func: function($http){
-<<<<<<< HEAD
         //let users=[];
         
         return {
-=======
-       
-        return{
->>>>>>> d8be259efb7db341e84bf90d46831ac66d48d5b7
 
             showUsers: function(user_name){
                    console.log('hello');
